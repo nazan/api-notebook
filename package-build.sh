03cb7698a4602dc4defd47dba6c8581700ca8a56
Sebastian Sampaoli <sebastian.sampaoli@mulesoft.com>
2016-09-01 16:17:45 -0300

    Add node_modules to docker build and grunt packages

--- package-build.sh
+++ package-build.sh
@@ -7,7 +7,7 @@ npm prune --production && \
 rm -rf ./output && \
 mkdir ./output && \
 cd ./build && \
-cp -R {./authenticate,./font,./images,./scripts,./styles,embed.html,embedded.html,favicon.ico,index.html} ../output && \
+cp -R {./authenticate,./font,./images,./scripts,./node_modules,./styles,embed.html,embedded.html,favicon.ico,index.html} ../output && \
 cd ../ && \
 tar -czvf api-notebook.tar.gz -C ./output . && \
 mv api-notebook.tar.gz ./output
