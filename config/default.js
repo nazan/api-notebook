6ec283412afcfba727b178d5412779eba84bb58a
Joaquin Bucca <joaquin.bucca@mulesoft.com>
2017-09-20 11:46:45 -0300

    just leave default and get variables from node env variables, set by k8s

--- config/default.js
+++ config/default.js
@@ -1,12 +1,20 @@
 module.exports = {
   application: {
-    url:           'http://localhost:3000',
-    title:         'API Notebook',
-    oauthCallback: '/authenticate/oauth.html'
+    url: process.env.URL ? process.env.URL :  'http://localhost:3000',
+    title:         process.env.TITLE ? process.env.TITLE : 'Notebook Site',
+    oauthCallback: process.env.OAUTH_CALLBACK ? process.env.OAUTH_CALLBACK : '/authenticate/oauth.html'
   },
   pkg: require('../package.json'),
   embed: {
-    script: 'http://localhost:3000/scripts/embed.js'
+    script: process.env.EMBED_SCRIPT ? process.env.EMBED_SCRIPT : 'http://localhost:3000/scripts/embed.js'
   },
-  plugins: {}
+  plugins: {
+    proxy: {
+      url: '/proxy'
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID ? process.env.GITHUB_CLIENT_ID : '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ? process.env.GITHUB_CLIENT_SECRET : ''
+    }
+  }
 };
