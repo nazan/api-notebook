e12a914b37f9c19d5a5e8e350cee65b803202516
Hussain Nazan Naeem <nazan@alliedinsure.com.mv>
2017-10-18 15:26:34 +0500

    setenv example file added.

---
+++ setenv-example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export NODE_ENV='development'
+export NODE_CONFIG_PERSIST_ON_CHANGE='N'
+export URL='http://localhost:3000'
+export TITLE='ABC - API Notebook'
+export EMBED_SCRIPT='http://localhost:3000/scripts/embed.js'
+export GITHUB_CLIENT_ID='abc'
+export GITHUB_CLIENT_SECRET='abc'
